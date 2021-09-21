21850832f01350c6bb52422f44e4a8738484856f
VickoFlores <82225605+VickoFlores@users.noreply.github.com>
2021-09-21 17:00:11 -0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,24 @@
+
+
+
+function Des1B(){
+    document.getElementById('Cargar-Mas-P').style.display= "block";
+    document.getElementById('VerMas1').style.display = "none";
+    document.getElementById('VerMasFle').style.display= "none";
+
+}
+
+function Des1N(){
+    document.getElementById('Cargar-Mas-P').style.display= "none";
+    document.getElementById('VerMas1').style.display= "block";
+    document.getElementById('VerMasFle').style.display= "block";
+}
+
+function Cerr1B(){
+    document.getElementById('Cerr1').style.display= "block";
+}
+
+function Cerr1N(){
+    document.getElementById('Cerr1').style.display= "none";
+}
+
